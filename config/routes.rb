a9564093e97746814521f4ab37b1d1e725ce998e
asahi1199 <asahi.yoshida.1130@gmail.com>
2022-10-04 08:50:01 +0000

    [Add] view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # configration of root
+  root 'homes#top'
+
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
 end
