65a92fb954bd0a39b7644080422be749362a9ff4
asahi1199 <asahi.yoshida.1130@gmail.com>
2022-10-06 15:45:29 +0000

    [Add] functions required

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,43 @@
 class BooksController < ApplicationController
   def create
-    book = Book.new(book_params)
-    book.save()
-    redirect_to '/'
+    @book = Book.new(book_params)
+
+    if @book.save()
+      flash[:notice] = "Book was successfully created."
+      redirect_to "/books/#{@book.id}"
+    else
+      @books = Book.all()
+      render :index
+    end
   end
 
   def index
     @book = Book.new()
+    @books = Book.all()
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy()
+    redirect_to "/books"
   end
 
   private
